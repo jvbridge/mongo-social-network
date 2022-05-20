16b9d608374d590da75af87ca0ad287bbd763906
Jonathan Bridge <jvbridge@gmail.com>
2022-05-19 17:49:46 -0700

    username and email

---
+++ models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: "Email address is required",
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, // email regex
+      "Please enter a valid email address",
+    ],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
