d465ee230aa195868a19706b96f446ac8c5c2922
Jonathan Bridge <jvbridge@gmail.com>
2022-05-20 21:51:13 -0700

    seeding thoughts

--- seed/seed.js
+++ seed/seed.js
@@ -2,13 +2,16 @@ const { Thought, User } = require("../models");
 const db = require("../config/connection");
 const { getRandomUsers } = require("./data");
 const ObjectId = require("mongoose").Types.ObjectId;
+const tweets = require("./tweets.json");
 
 // number of users we will generate
 const USER_COUNT = 50;
 // the minimum number of friends they will have, must be less than MAX_FRIENDS
 const MIN_FRIENDS = 5;
-// maximum number of friends, must be less than MIN_FRIENDS and USER_COUNT
+// maximum number of friends, must be > MIN_FRIENDS and < USER_COUNT
 const MAX_FRIENDS = 20;
+// minimum number of thoughts or "comments" each user is able to create
+const MIN_THOUGHTS = 5;
 
 // error handling
 db.on("error", (err) => err);
@@ -31,7 +34,7 @@ db.once("open", async () => {
   const friendPromises = [];
 
   // loop to give every user frineds
-  allUsers.forEach(async (user) => {
+  allUsers.forEach((user) => {
     // random number for how many friends they have
     const friendCount = Math.floor(Math.random() * MAX_FRIENDS) + MIN_FRIENDS;
 
@@ -62,6 +65,39 @@ db.once("open", async () => {
   // we finished setting up our updates, this puts them through
   await Promise.all(friendPromises);
 
+  // now that everyone has some friends lets give them some thoughts
+
+  // we need to get all our users again because the old array is out of date now
+  const allFriends = await User.find({});
+
+  // array for holding the promises of all the thoughts we will make.
+  const thoughtPromises = [];
+  allFriends.forEach((user) => {
+    // how many thoughts we can create will be randomly chosen
+    const thoughtCount =
+      Math.floor(Math.random() * (tweets.length - MIN_THOUGHTS)) + MIN_THOUGHTS;
+
+    // array for our thought strings
+    const thoughtStrings = [];
+    for (let i = 0; i < thoughtCount; i++) {
+      const newThought = tweets[Math.floor(Math.random() * tweets.length)];
+      // if we accidentally chose one we already are using, try again
+      if (thoughtStrings.includes(newThought)) {
+        i--;
+        continue;
+      }
+      thoughtStrings.push(newThought);
+    }
+    const thoughtObjs = thoughtStrings.map((thoughtText) => {
+      return { thoughtText, username: user.username };
+    });
+
+    // okay we've chosen our strings, lets set up the data insertion
+    thoughtPromises.push(Thought.collection.insertMany(thoughtObjs));
+  });
+
+  // setup complete, this executes its
+  await Promise.all(thoughtPromises);
   // we are now finished, exit node
   process.exit(0);
 });
