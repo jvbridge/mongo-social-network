5bbed015d07a4951d8e21fe2d0ac69d91a9f58bd
Jonathan Bridge <jvbridge@gmail.com>
2022-05-21 18:16:26 -0700

    refactored to use helper functions

--- seed/seed.js
+++ seed/seed.js
@@ -8,70 +8,69 @@ const tweets = require("./tweets.json");
 const USER_COUNT = 50;
 // the minimum number of friends they will have, must be less than MAX_FRIENDS
 const MIN_FRIENDS = 5;
-// maximum number of friends, must be > MIN_FRIENDS and < USER_COUNT
+/**
+ * The maximum number of friends
+ */
 const MAX_FRIENDS = 20;
 // minimum number of thoughts or "comments" each user is able to create
 const MIN_THOUGHTS = 5;
 
-// error handling
-db.on("error", (err) => err);
-
-// doing the seeding
-db.once("open", async () => {
-  // empty the whole database to start off
-  await Thought.deleteMany({});
-  await User.deleteMany({});
-
-  // CREATE USERS
-
-  // get a bunch of random unique users
+/**
+ * Helper function that creates all the users we need
+ */
+const createUsers = async () => {
   const users = getRandomUsers(USER_COUNT);
   console.table(users);
   await User.collection.insertMany(users);
+};
 
-  // ADD FRIENDS
-
-  // lets give them all friends now
-  // get references to them in the table
+/**
+ * Helper function to create all the friends of the users in question.
+ */
+const addFrineds = async () => {
+  // get references to all users in the database
   const allUsers = await User.find({});
-  // array to hold our promises for when we update each user
-  const friendPromises = [];
-
-  // loop to give every user frineds
+  // make all the users have empty friends arrays
   allUsers.forEach((user) => {
-    // random number for how many friends they have
-    const friendCount = Math.floor(Math.random() * MAX_FRIENDS) + MIN_FRIENDS;
+    user.frineds = [];
+  });
 
-    // make an array of friends to add
-    const friendArr = [];
-    // populate it with references
+  // iterating over all the users figuring out which friends to add
+  allUsers.forEach((user) => {
+    // the number of friends they're going to have
+    const friendCount = Math.floor(Math.random() * MAX_FRIENDS) + MIN_FRIENDS;
+    // make a list of friends to add
+    const toAdd = [];
     for (let i = 0; i < friendCount; i++) {
+      // choose a random user to be their friend
       const newFriend = allUsers[Math.floor(Math.random() * allUsers.length)];
-      // if it's not a valid friend try again
-      if (friendArr.includes(newFriend) || user == newFriend) {
+      // stops duplicates and adding yourself
+      if (toAdd.includes(newFriend) || user == newFriend) {
         i--;
         continue;
       }
-      // otherwise add it
-      friendArr.push(newFriend);
+      toAdd.push(newFriend);
     }
-
-    // add the whole new array and save it
-    const friendIds = friendArr.map((friend) => {
-      const id = new ObjectId(friend._id);
-      return id;
+    // populate the arrays for each friends
+    toAdd.forEach((friend) => {
+      // if we already are friends, skip it
+      if (user.friends.includes(friend.id)) return;
+      // otherwise add to each array
+      friend.frineds.push(user.id);
+      user.friends.push(friend.id);
     });
-    // add it to the promise array
-    friendPromises.push(
-      User.findOneAndUpdate({ _id: user._id }, { friends: friendIds })
-    );
   });
-  // we finished setting up our updates, this puts them through
+  // array to hold our promises for when we update each user
+  const friendPromises = allUsers.map((user) => user.save());
+  // then execute the save
   await Promise.all(friendPromises);
+};
 
-  // CREATE THOUGHTS
-
-  // we need to get all our users again because the old array is out of date now
+/**
+ * helper function to create all the thoughts for all users
+ */
+const createThoughts = async () => {
+  // get all the users
   const allFriends = await User.find({});
 
   // array for holding the promises of all the thoughts we will make.
@@ -110,8 +109,33 @@ db.once("open", async () => {
       User.findOneAndUpdate({ _id: user.id }, { thoughts: thoughtIds })
     );
   });
-  // setup complete, this executes the data insertion and update
+  // setup complete, this executes the data insertion and updates
   await Promise.all(thoughtPromises);
+};
+
+// error handling
+db.on("error", (err) => err);
+
+// doing the seeding
+db.once("open", async () => {
+  // empty the whole database to start off
+  await Thought.deleteMany({});
+  await User.deleteMany({});
+
+  // helper function to create all the users
+  await createUsers();
+
+  // helper function to create all the friend relationships
+  await addFrineds();
+
+  // helper function to create all the thoughts for the posts
+  await createThoughts();
+
+  // we need to get all our users again because the old array is out of date now
+
+  // CREATE REACTIONS
+  // once again we need to get an updated list
+  const allThoughts = await User.find({});
 
   // we are now finished, exit node
   process.exit(0);
