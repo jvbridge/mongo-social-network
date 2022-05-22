3dc10d07d0dbb9d170627093c5922097c89fb31b
Jonathan Bridge <jvbridge@gmail.com>
2022-05-22 13:32:10 -0700

    allows you to get users by username

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User, Thought } = require("../../models");
 
+// get all users
 router.get("/", async (req, res) => {
   try {
     const users = await User.find({});
@@ -10,8 +11,39 @@ router.get("/", async (req, res) => {
   }
 });
 
-// TODO: POST a new user
+// Create a new user
+router.post("/", async (req, res) => {
+  try {
+    if (!req.body.email || !req.body.username) {
+      res.status(400).json("malformed body");
+      return;
+    }
+    const newUser = {
+      email: req.body.email,
+      username: req.body.username,
+    };
+    console.log("new user: ", newUser);
+    // check if the user's email and password are already in the database
+    const usernameExists = await User.find({ username: newUser.username });
+    if (usernameExists.length > 0) {
+      res.status(400).json({ message: "User already exists:", usernameExists });
+      return;
+    }
+    const emailExists = await User.find({ email: newUser.email });
+    if (emailExists.length > 0) {
+      res.status(400).json({ message: "User already exists:", emailExists });
+      return;
+    }
 
+    // all clear lets make the user
+    const newUserData = await User.create({ ...newUser });
+    res.status(200).json(newUserData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get a specific user by id
 router.get("/:id", async (req, res) => {
   try {
     const userData = await User.findById(req.params.id, [
@@ -48,6 +80,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// get a specific user's friends
 router.get("/:id/friends/", async (req, res) => {
   try {
     const user = await User.findById(req.params.id, "friends");
@@ -58,6 +91,20 @@ router.get("/:id/friends/", async (req, res) => {
     console.log("user: ", user);
     const friends = await User.find({ _id: user.friends });
     res.status(200).json(friends);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get a specific user by username
+router.get("/u/:username", async (req, res) => {
+  try {
+    const user = await User.find({ username: req.params.username });
+    if (!user) {
+      res.sendStatus(404);
+      return;
+    }
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
